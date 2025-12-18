539b643ab8388b5996c41036c5bd41c559dc1af5
besufikad kasahun <besufikadkasahun12@gmail.com>
2025-12-18 10:48:09 -0800

    edit manege.py

--- todo_backend/manage.py
+++ todo_backend/manage.py
@@ -2,15 +2,6 @@
 """Django's command-line utility for administrative tasks."""
 import os
 import sys
-import django
-from django.contrib.auth import get_user_model
-
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'todo_backend.settings')
-django.setup()
-
-User = get_user_model()
-if not User.objects.filter(username="admin").exists():
-    User.objects.create_superuser("admin", "admin@example.com", "12345")
 
 
 def main():
